acddf799588aba896a3c60a54fc602db510bff9d
unknown <vinithkaradi747@gmail.com>
2022-11-11 17:30:24 +0530

    Feature: Added toggle and delete functions

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ function Book(title, author, numOfPages, readingStatus) {
     this.readingStatus = readingStatus;
 }
 
+let bookOne = new Book('Mother of Learning','Domagoj Kurmaic',1932,true);
+let bookTwo = new Book('Lord of Mysteries','Cuttlefish',1430,true);
+myLibrary.push(bookOne);
+myLibrary.push(bookTwo);
+displayList();
+
 function openBookForm() {
     modalContainer[0].style.display = 'grid';
 }
@@ -21,8 +27,8 @@ addBookForm.addEventListener('submit',(event) => {
         addBookForm.numOfPages.value, addBookForm.readingStatus.checked);
     myLibrary.push(newBook);
     closeForm();
-    addBook();
-    console.log(myLibrary);
+    displayList();
+    //console.log(myLibrary);
     return;
 });
 
@@ -32,8 +38,63 @@ function closeForm() {
     return;
 }
 
-function addBook() {
+function displayList() {
+    var tableElement = document.querySelector('table');
+    tableElement.innerHTML = '<tr><th>Title</th><th>Author</th><th>Number of Pages</th><th>Unread / Read</th></tr>';
+    myLibrary.forEach((book) => {
+        var newRow = document.createElement('tr');
+        tableElement.append(newRow);
+        for (var prop in book) {
+            var dataNode = document.createElement('td');
+            if (prop === 'readingStatus') {
+                var labelNode = document.createElement('label');
+                labelNode.setAttribute('class','switch');
+                dataNode.append(labelNode);
+                var checkInputNode = document.createElement('input');
+                checkInputNode.setAttribute('type','checkbox');
+                checkInputNode.setAttribute('name','readingStatus');
+                checkInputNode.setAttribute('data-bookindex',myLibrary.indexOf(book));
+                if (book[prop]) {
+                    checkInputNode.checked = true;
+                }
+                insertReadingStatusTogglingFunction(checkInputNode);
+                labelNode.append(checkInputNode);
+                var spanNode = document.createElement('span');
+                spanNode.setAttribute('class','slider round');
+                labelNode.append(spanNode);
+            } else {
+                dataNode.textContent = book[prop].toLocaleString('en-us');
+            }
+            newRow.append(dataNode);
+        }
+        var delDataNode = document.createElement('td');
+        var delIconNode = document.createElement('i');
+        delIconNode.className = 'material-symbols-rounded del-icon';
+        delIconNode.setAttribute('data-bookindex',myLibrary.indexOf(book));
+        delIconNode.innerHTML = '&#xe872';
+        insertDeleteFunction(delIconNode);
+        delDataNode.append(delIconNode);
+        newRow.append(delDataNode);
+    })
     return;
 }
 
-//function toggleReadStatus(){}
+function insertReadingStatusTogglingFunction(inputElement) {
+    inputElement.addEventListener('click',() => {
+        var indexOfBookInArray = inputElement.dataset.bookindex;
+        var bookofIndex = myLibrary[indexOfBookInArray];
+        bookofIndex.readingStatus = !(bookofIndex.readingStatus);
+        //console.log(myLibrary);
+        return; 
+    })
+}
+
+function insertDeleteFunction(delElement) {
+    delElement.addEventListener('click',() => {
+        var indexOfBookInArray = delElement.dataset.bookindex;
+        //console.log(indexOfBookInArray);
+        myLibrary.splice(indexOfBookInArray,1);
+        displayList();
+        return;
+    })
+}
